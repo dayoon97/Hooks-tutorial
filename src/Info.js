56f0dbd12a0e8f752db06074a11d1ea7d7a03268
dayoon97 <ekdbsekdbs@gmail.com>
2020-12-22 17:13:18 +0900

    react hooks 튜토리얼
    useEffect 학습 및 정리중

--- src/Info.js
+++ src/Info.js
@@ -4,8 +4,12 @@ const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
   useEffect(() => {
-    console.log(name);
-  }, [name]);
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname
+    });
+  });
 
   const onChangeName = e => {
     setName(e.target.value);
